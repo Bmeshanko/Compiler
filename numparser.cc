e1932c2fa5debf56a66de04c179eb879ab27e989
Bmeshanko <bmeshank@purdue.edu>
2022-05-04 15:52:07 -0400

    Commit from Makefile

---
+++ numparser.cc
@@ -0,0 +1,15 @@
+#include <vector>
+#include "numparser.hh"
+
+Numparser::Numparser() {
+	nums = std::vector<int>();
+	ops = std::vector<char>();
+}
+
+void insertNum(int num) {
+	nums.push_back(num);
+}
+
+void insertOp(char op) {
+	ops.push_back(op);
+}
