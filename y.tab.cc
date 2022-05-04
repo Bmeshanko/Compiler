0c9e8fd1d42a2a38ea774bfbf0bc824e17ede350
Bmeshanko <bmeshank@purdue.edu>
2022-05-04 16:04:25 -0400

    Commit from Makefile

--- y.tab.cc
+++ y.tab.cc
@@ -1325,7 +1325,7 @@ yyreduce:
 #line 35 "compiler.y"
                {
 		// Declare new Numparser to process Equations.
-		Compiler::_numparser = new Numparser();
+		Compiler::_numparser = Numparser();
 		std::string raw = std::string (*(yyvsp[0].num_string));
 		char * str = (char *) raw.c_str();
 		int num = atoi(str);
