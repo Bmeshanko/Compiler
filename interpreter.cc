adc4ab1c924b7d45c7535b18065607f203c89ba3
Bmeshanko <bmeshank@purdue.edu>
2023-06-02 18:25:44 -0400

    Almost done w/ interpreter

--- interpreter.cc
+++ interpreter.cc
@@ -5,26 +5,53 @@
 
 map<char *, int> variables;
 
-void eval(struct Env *prog) {
-    
+void eval(struct Env *prog, int start, int end) {
+    for (int i = start; i < end; i++) {
+        struct Tree * tree = prog->prog[i];
+        short type = tree->type;
+
+        if (type == 3) {
+            // Let Statement
+            evalLet((struct Let *) tree);
+        } else if (type == 5) {
+            // If Statement
+            int end = findEnd(prog, i);
+            if (evalCond((struct If *)tree->cond), 5) {
+                eval(prog, i + 1, end);
+            }
+        } else if (type == 6) {
+            // While Statement
+            int end = findEnd(prog, i);
+            while (evalCond((struct While *)tree->cond), 6) {
+                eval(prog, i + 1, end);
+            }
+        } else if (type == 8) {
+            // Print Statement
+        } else {
+            // Do nothing
+        }
+    }
 }
 
-void evalGeneric(struct Tree *tree) {
-
-}
-
-void evalFromTo(struct Env *prog, int start, int end) {
-
+int findEnd(struct Env *prog, int start) {
+    int diff = 1;
+    for (int i = start + 1; i < prog->lines; i++) {
+        short type = prog->prog[i]->type;
+        if (type == 7) diff--;
+        if (type == 4 || type == 5) diff++;
+        if (diff == 0) return i;
+    }
+    return -1;
 }
 
 void evalLet(struct Let *let) {
      variables[let->id] = evalNum(let->val);
 }
 
-bool evalCond(struct Tree *tree) {
-    if (tree -> type == 5) {
+bool evalCond(struct Tree *tree, short type) {
+    if (type == 5) {
         return evalNum((struct If *) tree -> cond) == 0;
-    } else if (tree -> type == 6) {
+    } else if (type == 6) {
         return evalNum((struct While *) tree -> cond) == 0;
     } else {
         return NULL;
