69d39b3ff0a58fb246b44b4cb014412d64fb1004
Bmeshanko <bmeshank@purdue.edu>
2023-06-09 10:07:45 -0400

    Wrote type classes

--- semantics.hh
+++ semantics.hh
@@ -1,5 +1,6 @@
 #include "parser.hh"
 #include <bits
+using namespace std;
 
 class Type {
     std::string type;
@@ -7,10 +8,32 @@ class Type {
         Type(std::string type) {
             this.type = type;
         }
+
+        toString() {
+            return type;
+        }
 }
 
 Type IntType = new Type("Int");
 Type CharType = new Type("Char");
+
+class FunType : Type {
+    vector<pair<std::string, Type>> args;
+
+    public:
+        Type(vector<pair<std::string, Type>> args) {
+            this.args = args;
+        }
+
+        toString() {
+            std::string ret = "(";
+            for (arg : args) {
+                ret += "[" + arg.first() + ", " + arg.second + "]";
+            }
+            ret += ")";
+            return ret;
+        }
+}
 
 enum Type typeInfer(struct Tree * tree);
 void analyzeSemantics(struct Env * prog);
